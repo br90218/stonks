fad9339170895a67920fc9ed0be0efece30c024d
Brian Teng <askmeifyouwantmyusername@gmail.com>
2025-02-24 01:22:25 -0500

    set up for pause menu overlay

--- src/renderer/src/components/Panels/ControlsPanel.tsx
+++ src/renderer/src/components/Panels/ControlsPanel.tsx
@@ -1,11 +1,28 @@
 import styles from '@renderer/assets/css/components/panels/controlspanel.module.css';
+import { useState } from 'react';
+import { createPortal } from 'react-dom';
 
 export default function ControlsPanel(): JSX.Element {
+    const [paused, setPaused] = useState(false);
     return (
         <div className={styles.wrapper}>
-            <button>Config</button>
+            <button onClick={() => setPaused(true)}>Pause</button>
             <button>Help</button>
             <button>Quit</button>
+            {paused &&
+                createPortal(
+                    <ModalContent onClose={() => setPaused(false)} />,
+                    document.getElementById('overlay')!
+                )}
+        </div>
+    );
+}
+
+function ModalContent(props: { onClose: () => void }): JSX.Element {
+    return (
+        <div className="modal">
+            <div> something</div>
+            <button onClick={props.onClose}>Click me to close</button>
         </div>
     );
 }
