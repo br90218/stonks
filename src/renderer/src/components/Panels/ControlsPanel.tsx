70dc13e691ba54beb574bba16323d28bb2becc7f
Brian Teng <askmeifyouwantmyusername@gmail.com>
2025-02-26 12:49:06 -0500

    use portals to create popups

--- src/renderer/src/components/Panels/ControlsPanel.tsx
+++ src/renderer/src/components/Panels/ControlsPanel.tsx
@@ -1,6 +1,7 @@
 import styles from '@renderer/assets/css/components/panels/controlspanel.module.css';
 import popups from '@renderer/assets/css/components/popups.module.css';
 import { useState } from 'react';
+import { createPortal } from 'react-dom';
 
 export default function ControlsPanel(): JSX.Element {
     const [paused, setPaused] = useState<boolean>(false);
@@ -9,23 +10,23 @@ export default function ControlsPanel(): JSX.Element {
             <button onClick={() => setPaused(true)}>Pause</button>
             <button>Help</button>
             <button>Quit</button>
-            {/* {paused &&
+            {paused &&
                 createPortal(
                     <ModalContent onClose={() => setPaused(false)} />,
                     document.getElementById('overlay')!
-                )} */}
-            <div className={popups.popup} id="pause" style={{ display: paused ? 'block' : 'none' }}>
+                )}
+            {/* <div className={popups.popup} id="pause" style={{ display: paused ? 'block' : 'none' }}>
                 something
-            </div>
+            </div> */}
         </div>
     );
 }
 
-// function ModalContent(props: { onClose: () => void }): JSX.Element {
-//     return (
-//         <div className="modal">
-//             <div> something</div>
-//             <button onClick={props.onClose}>Click me to close</button>
-//         </div>
-//     );
-// }
+function ModalContent(props: { onClose: () => void }): JSX.Element {
+    return (
+        <div className={`${popups.popup} modal`}>
+            <div> something</div>
+            <button onClick={props.onClose}>Click me to close</button>
+        </div>
+    );
+}
